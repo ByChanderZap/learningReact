53476e82d5de4accd7f70c83fcc2349a17e9db80
alexander herrera <chander.zap@gmail.com>
2023-07-14 20:47:50 -0600

    linter and stuff

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -2,18 +2,15 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App() {
-  const bcrra = {
-    username: 'bcrro',
-    isFollowing: false,
-  }
-  const jorge = {
-    username: 'jorge',
-    isFollowing: false,
-  }
-  const alejandrocapo = {
-    username: 'alexelcapo',
-    isFollowing: true,
-  }
+  const bcrra = { username: 'bcrro', isFollowing: false }
+  // const jorge = {
+  //   username: 'jorge',
+  //   isFollowing: false,
+  // }
+  // const alejandrocapo = {
+  //   username: 'alexelcapo',
+  //   isFollowing: true,
+  // }
   return (
     <section className="App">
       <TwitterFollowCard username={bcrra.username}>becerraaaa</TwitterFollowCard>
